60eca339735f308c9ab2e700a1603ca5f3a401e4
Alexandr <86582717+tseneven@users.noreply.github.com>
2025-09-27 15:21:24 +0500

    f

--- src/mainusers/mainusers.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MainusersService } from './mainusers.service';
-import { MainusersController } from './mainusers.controller';
-
-@Module({
-  controllers: [MainusersController],
-  providers: [MainusersService],
-})
-export class MainusersModule {}
